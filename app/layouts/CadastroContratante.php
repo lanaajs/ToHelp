329ba9a2d55ecebbc7a906224e8fe3bbbdffb5a1
minksouza <yasmin.rodrigues2112@gmail.com>
2023-10-21 20:36:47 -0300

    novas paginas de login e cadastro

--- app/layouts/CadastroContratante.php
+++ app/layouts/CadastroContratante.php
@@ -1,42 +1,40 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<!-- Coding By CodingNepal - codingnepalweb.com -->
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="../../public/assets/css/CadastroContratante.css">
+
+    <!----===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
-    <link rel="stylesheet" href="../../public/assets/css/CadastroContratante.css">
-
-    <link rel="icon" type="image/x-icon" href="../../public/assets/img/LogoPreta.png">
-    <title>To Help - Cadastro</title>
+    <title>ToHelp Cadastro</title>
 </head>
 
 <body>
 
-    <header id="cabecalho">
-
-        <div id="logo">
-            <img src="../../public/assets/img/LogoBranca.png" alt="">
+    <div class="tudo">
+        <div class="total">
+            <div id="logo">
+                <img src="../../public/assets/img/LogoBranca.png" alt="">
+            </div>
+            <div id="txt">
+                <p class="p1">Sua Saúde é</p>
+                <p class="p2">Nossa Prioridade.</p>
+                <br>
+                <p class="p3">ToHelp, para <br> ajudar.</p>
+            </div>
         </div>
-
-        <div id="setinha">
-            <a href="index.html"><img src="../../public/assets/img/iconamoon_arrow-up-2-duotone.png" alt=""></a>
-        </div>
-
-    </header>
-
-    <!-- Login -->
-    <main>
-        <section id="form">
-
-            <h1 id="h1">Cadastre-se</h1>
-
-            <div class="divArea">
+        <div class="container">
+            <div class="cad">
+                <header>Cadastro</header>
                 <div id="area">
                     <div id="areaP1" onclick="contratante()">
                         <span class="material-icons">elderly</span>
@@ -48,119 +46,98 @@
                     </div>
                 </div>
             </div>
-
-            <!-- Formulario -->
-            <form action="">
-
-                <div class="divArea">
-                    <div>
-                        <div id="checkbox">
-                            <label>
-                                <input type="checkbox" id="check" name="escolha" value="1" onclick="textBox(this)">
-                                <span class="checkmark"></span>
-                            </label>
-                            <br>
-                            <p>Sou responsável por mim e desejo me cadastrar</p>
+            <form action="#">
+                <div class="form first">
+                    <div class="details personal">
+                        <span class="title">Informações Pessoais</span>
+                        <div class="fields">
+                            <div class="input-field">
+                                <label>Nome</label>
+                                <input type="text" placeholder="Insira seu nome" required>
+                            </div>
+                            <div class="input-field">
+                                <label>Sobrenome</label>
+                                <input type="text" placeholder="Insira seu sobrenome" required>
+                            </div>
+                            <div class="input-field">
+                                <label>CPF</label>
+                                <input type="text" placeholder="Insira seu CPF" required>
+                            </div>
+                            <div class="input-field">
+                                <label>RG</label>
+                                <input type="text" placeholder="Insira seu RG" required>
+                            </div>
+                            <div class="input-field">
+                                <label>Date de Nascimento</label>
+                                <input type="date" placeholder="Insira sua data de nascimento" required>
+                            </div>
+                            <div class="input-field">
+                                <label>Email</label>
+                                <input type="text" placeholder="Insira seu email" required>
+                            </div>
+                            <div class="input-field">
+                                <label>Celular</label>
+                                <input type="number" placeholder="Insira seu telefone celular" required>
+                            </div>
+                            <div class="input-field">
+                                <label>Gênero</label>
+                                <select required>
+                                    <option disabled selected>Selecione o gênero</option>
+                                    <option>Masculino</option>
+                                    <option>Feminino</option>
+                                    <option>Outros</option>
+                                </select>
+                            </div>
+                            <div class="input-field">
+                                <label>CEP</label>
+                                <input type="text" placeholder="Insira seu CEP" required>
+                            </div>
+                            <div class="input-field">
+                                <label>Estado</label>
+                                <input type="text" placeholder="Insira um estado" required>
+                            </div>
+                            <div class="input-field">
+                                <label>Cidade</label>
+                                <input type="text" placeholder="Insira uma cidade" required>
+                            </div>
+                            <div class="input-field">
+                                <label>Número</label>
+                                <input type="text" placeholder="Insira um número" required>
+                            </div>
                         </div>
-                        <div id="checkbox2">
-                            <label>
-                                <input type="checkbox" id="check2" name="escolha" value="2" onclick="textBox(this)">
-                                <span class="checkmark"></span>
-                            </label>
-                            <br>
-                            <p>Sou responsável legal e desejo cadastrar meu dependente</p>
+                    </div>
+                    <div class="details ID">
+                        <div class="fields">
+                            <div class="input-field">
+                                <label>Complemento</label>
+                                <input type="text" placeholder="Insira um complemento" required>
+                            </div>
+                            <div class="input-field">
+                                <label>Senha</label>
+                                <input id="senha" type="text" placeholder="Crie uma senha" required>
+                            </div>
+                            <div class="input-field">
+                                <label>Selecione uma opção</label>
+                                <div class="mark"><input type="radio" id="check" name="escolha" value="1"
+                                        onclick="textBox(this)">
+                                    <p>Sou responsável por mim e desejo me cadastrar</p>
+                                </div>
+                                <div class="mark" id="markdois"><input type="radio" id="check" name="escolha" value="2"
+                                        onclick="textBox(this)">
+                                    <p> Sou responsável legal e desejo cadastrar meu dependente</p>
+                                </div>
+                            </div>
                         </div>
+                        <div class="btnn">                         
+                            <button class="nextBtn">
+                                <span class="btnText">Prosseguir</span>
+                                <i class="uil uil-navigator"></i>
+                            </button>
+                            <p style="color: black;">Já possui cadastro? <a href="">Faça Login </a></p>
+                    </div>
                     </div>
                 </div>
-
-                <div id="inputs">
-
-                    <div class="divInputs">
-                        <label for="nome">Nome Completo</label>
-                        <br>
-                        <input class="input" type="text" id="nome">
-
-                        <br><br>
-
-                        <label for="email">E-mail</label>
-                        <br>
-                        <input class="input" type="email" id="email">
-
-                        <br><br>
-
-                        <label for="tel">Telefone</label>
-                        <br>
-                        <input class="input" type="text" id="tel" maxlength="14" placeholder="(00) 00000-0000"
-                        onkeypress="return event.charCode >= 48 && event.charCode <= 57">
-
-                        <div id="dependente">
-                            <br>
-                            <label for="dependentes">Relação com o dependente</label>
-                            <br>
-                            <input class="input" type="text" id="dependentes">
-                        </div>
-                    </div>
-
-                    <br>
-
-                    <div class="divInputs">
-                        <label for="cpf">CPF</label>
-                        <br>
-                        <input class="input" type="text" id="cpf" maxlength="14" placeholder="000.000.000-00" 
-                        onkeypress="return event.charCode >= 48 && event.charCode <= 57">
-                        <br><br>
-
-                        <div class="input-area">
-
-                            <div id="inputs-special">
-                                <label for="state">Estado</label>
-                                <input class="input" type="text" id="state">
-                            </div>
-
-                            <div id="inputs-state-city">
-                                <label for="city">Cidade</label>
-                                <input class="input" type="text" id="city">
-                            </div>
-
-                        </div>
-
-                        <br>
-
-                        <label for="endereco">Endereço</label>
-                        <br>
-                        <input class="input" type="text" id="endereco">
-
-                        <br><br>
-
-                        <div class="input-area">
-
-                            <div id="inputs-special">
-                                <label for="numero">Número</label>
-                                <input class="input" type="text" id="numero" 
-                                
-                                onkeypress="return event.charCode >= 48 && event.charCode <= 57">
-                            </div>
-
-                            <div id=" inputs-state-city">
-                                <label for="complemento">Complemento</label>
-                                <input type="text" class="input" id="complemento">
-                            </div>
-
-                        </div>
-                    </div>
-
-                </div>
-
-
-
-            </form>
-            <div id="botao">
-                <button id="btn" onclick="analisar()">Cadastrar-se</button>
-            </div>
-        </section>
-    </main>
-    <script src="../../public/assets/js/Cadastro.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+        </div>
+    </div>
 </body>
-
 </html>
--- app/layouts/LoginContratante.php
+++ app/layouts/LoginContratante.php
@@ -1,98 +1,79 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<!-- Coding By CodingNepal - codingnepalweb.com -->
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!----======== CSS ======== -->
+    <link rel="stylesheet" href="../../public/assets/css/LoginContratante.css">
+
+    <!----===== Iconscout CSS ===== -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    
-    <link rel="stylesheet" href="../../../public/assets/css/LoginContratante.css">
-    <script src="../../public/assets/js/Login.js"></script>
-
-    <link rel="icon" type="image/x-icon" href="../../public/assets/img/LogoPreta.png">
-    <title>ToHelp - Login</title>
+    <title>Tohelp Login</title>
 </head>
 
 <body>
-    <!-- Cabeçalho -->
-    <header id="cabecalho">
+    <div class="tudo">
 
-        <div id="logo">
-            <img src="../../public/assets/img/LogoBranca.png" alt="">
-        </div> 
-        
-        <div id="setinha">
-            <a href="index.html"><img src="../../public/assets/img/iconamoon_arrow-up-2-duotone.png" alt=""></a>
+        <div class="total">
+            <div id="logo">
+                <img src="../../public/assets/img/LogoBranca.png" alt="">
+            </div>
+            <div id="txt">
+                <p class="p1">Sua Saúde é</p>
+                <p class="p2">Nossa Prioridade.</p>
+                <br>
+                <p class="p3">ToHelp, para <br> ajudar.</p>
+            </div>
         </div>
-        
 
-    </header>
-
-    <!-- Textos -->
-    <section id="txt">
-
-        <p class="p1">Sua Saúde é</p>
-        <p class="p2">Nossa Prioridade.</p>
-        <br>
-        <p class="p3">ToHelp, para <br> ajudar.</p>
-
-    </section>
-
-    <!-- Login -->
-    <section id="respon">
-        <section id="form">
-
-            <h1 id="h1">Login</h1>
-
-            <div id="area">
-                <div id="areaP1" onclick="contratante()">
-                    <span class="material-icons">elderly</span>
-                    <p>Contratante</p>
-                </div>
-                <div id="areaP2" onclick="cuidador()">
-                    <span class="material-icons">&#xE87C;</span>
-                    <p>Cuidador</p>
-                </div>
-            </div>
-
-            <!-- Formulario -->
-            <div id="inputs">
-                <form action="">
-
-                    <label for="cpf">CPF</label>
-                    <br>
-                    <input type="text" id="cpf">
-                    <br><br>
-                    <label for="senha">Senha</label>
-                    <br>
-                    <input type="password" id="senha">
-
-                    <div id="botao">
-                        <input type="button" onclick="logarContratante()" value="Entrar" id="btn">
+        <div class="container">
+            <div class="cad">
+                <header>Login</header>
+                <div id="area">
+                    <div id="areaP1" onclick="contratante()">
+                        <span class="material-icons">elderly</span>
+                        <p>Contratante</p>
                     </div>
-                    
-                </form>
+                    <div id="areaP2" onclick="cuidador()">
+                        <span class="material-icons">&#xE87C;</span>
+                        <p>Cuidador</p>
+                    </div>
+                </div>
             </div>
 
-            <div id="textC">
-                <p>Ainda não possui Login? <a href="/cadastro/contratante" id="textCL">Cadastre-se</a></p> 
-            </div>
-            
-        </section>
+            <form action="#">
+                <div class="form first">
+                    <div class="details personal">
+                        <div class="fields">
+                            <div class="input-field">
+                                <label>CPF</label>
+                                <input type="text" placeholder="Insira seu CPF">
+                            </div>
 
-        <section id="txt2">
-
-            <p class="p1">Sua Saúde é</p>
-            <p class="p2">Nossa Prioridade.</p>
-            <br>
-            <p class="p3">ToHelp, para <br> ajudar.</p>
-    
-        </section>
-
-    </section>
-    
+                            <div class="input-field">
+                                <label>Senha</label>
+                                <input id="senha" type="text" placeholder="Insira sua senha">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="btnn">
+                        <button class="nextBtn">
+                            <span class="btnText">Entrar</span>
+                            <i class="uil uil-navigator"></i>
+                        </button>
+                        <p style="color: black;">Não possui login? <a href="">Faça o cadastro</a></p>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
