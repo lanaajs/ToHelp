9fe71733e87cd121694bcd008aaa64b340ed3bda
minksouza <yasmin.rodrigues2112@gmail.com>
2023-10-26 23:48:45 -0300

    nova página de buscar cuidadores

---
+++ app/layouts/BuscarCuidador.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link id="u-theme-google-font" rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Ubuntu:300,300i,400,400i,500,500i,700,700i">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="../../public/assets/css/BuscarCuidador.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <a id="linkperfil" href="/contratante/conta/1">
+            <span class="material-symbols-outlined">
+                account_circle
+            </span>
+        </a>
+    </header>
+    <section class="sec1">
+        <div class="tudo1">
+            <img id="logo" src="../../public/assets/img/LogoPreta.png" alt="">
+            <div class="txt1">
+                <h1>Busque Cuidadores</h1>
+                <p>Nossa equipe é composta por profissionais altamente qualificados e dedicados, comprometidos em
+                    proporcioContratar alto padrão de cuidados para seus entes queridos.</p>
+            </div>
+            <hr>
+            <div class="txt2">
+                <div class="subtxt">
+                    <div class="circle">.</div>
+                    <p id="pdif">
+                        Nossa plataforma oferece um processo de seleção rigoroso, assegurando que todos os cuidadores
+                        são de confiança e possuem as habilidades necessárias para atender às necessidades específicas
+                        de cada indivíduo.</p>
+                </div>
+                <div class="subtxt">
+                    <div class="circle">.</div>
+                    <p id="pdif">
+                        Nossa plataforma oferece um processo de seleção rigoroso, assegurando que todos os cuidadores
+                        são de confiança e possuem as habilidades necessárias para atender às necessidades específicas
+                        de cada indivíduo.</p>
+                </div>
+            </div>
+        </div>
+        <div class="tudo2">
+            <img id="foto" src="../../public/assets/img/cuidador_idosos_2.jpg" alt="">
+        </div>
+    </section>
+    <section class="sec2">
+        <div class="topo">
+            <h1 class="titulo-sec2">Encontre os melhores <br> cuidadores aqui</h1>
+            <div class="input">
+                <input id="inp" type="text">
+                <span class="material-icons">search</span>
+            </div>
+        </div>
+
+        <div class="cuidadores1">
+            <div class="cuidadores2">
+                <div class="cuidcard">
+                    <img id="cuidft" src="../../public/assets/img/yy.jpg" alt="">
+                    <div class="txtcard">
+                        <h2>Maria Lívia Barbosa</p>
+                            <h3>Técnica em enfermagem</p>
+                                <p id="pdif">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis
+                                    glavrid from dolor amet iquam lorem bibendum</p>
+                    </div>
+                    <button id="saiba">Contratar</button>
+                </div>
+                <div class="cuidcard">
+                    <img id="cuidft" src="../../public/assets/img/yy.jpg" alt="">
+                    <div class="txtcard">
+                        <h2>Maria Lívia Barbosa</p>
+                            <h3>Técnica em enfermagem</p>
+                                <p id="pdif">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis
+                                    glavrid from dolor amet iquam lorem bibendum</p>
+                    </div>
+                    <button id="saiba">Contratar</button>
+                </div>
+                <div class="cuidcard">
+                    <img id="cuidft" src="../../public/assets/img/yy.jpg" alt="">
+                    <div class="txtcard">
+                        <h2>Maria Lívia Barbosa</p>
+                            <h3>Técnica em enfermagem</p>
+                                <p id="pdif">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis
+                                    glavrid from dolor amet iquam lorem bibendum</p>
+                    </div>
+                    <button id="saiba">Contratar</button>
+                </div>
+                <div class="cuidcard">
+                    <img id="cuidft" src="../../public/assets/img/yy.jpg" alt="">
+                    <div class="txtcard">
+                        <h2>Maria Lívia Barbosa</p>
+                            <h3>Técnica em enfermagem</p>
+                                <p id="pdif">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis
+                                    glavrid from dolor amet iquam lorem bibendum</p>
+                    </div>
+                    <button id="saiba">Contratar</button>
+                </div>
+                <div class="cuidcard">
+                    <img id="cuidft" src="../../public/assets/img/yy.jpg" alt="">
+                    <div class="txtcard">
+                        <h2>Maria Lívia Barbosa</p>
+                            <h3>Técnica em enfermagem</p>
+                                <p id="pdif">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis
+                                    glavrid from dolor amet iquam lorem bibendum</p>
+                    </div>
+                    <button id="saiba">Contratar</button>
+                </div>
+                <div class="cuidcard">
+                    <img id="cuidft" src="../../public/assets/img/yy.jpg" alt="">
+                    <div class="txtcard">
+                        <h2>Maria Lívia Barbosa</p>
+                            <h3>Técnica em enfermagem</p>
+                                <p id="pdif">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis
+                                    glavrid from dolor amet iquam lorem bibendum</p>
+                    </div>
+                    <button id="saiba">Contratar</button>
+                </div>
+                <div class="cuidcard">
+                    <img id="cuidft" src="../../public/assets/img/yy.jpg" alt="">
+                    <div class="txtcard">
+                        <h2>Maria Lívia Barbosa</p>
+                            <h3>Técnica em enfermagem</p>
+                                <p id="pdif">Glavi amet ritnisl libero molestie ante ut fringilla purus eros quis
+                                    glavrid from dolor amet iquam lorem bibendum</p>
+                    </div>
+                    <button id="saiba">Contratar</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <p class="u-text">
+            <span>Todos os direitos reservados - </span>
+        </p>
+        <a class="u-link" href="/" target="_blank">
+            <span>ToHelp</span>
+        </a>
+    </footer>
+
+
+
+
+</body>
+
+</html>
