1600472dce1f889e223de0e93916505a8493d145
Davi Dorea <ddorea65@gmail.com>
2023-11-17 09:49:11 -0300

    video no carrosel do perfil

--- app/layouts/BuscarCuidador.php
+++ app/layouts/BuscarCuidador.php
@@ -188,46 +188,6 @@ include("../controllers/BuscarCuidController.php");
         </a>
     </footer>
 
-    <script>
-        var videosEmReproducao = false; // Variável para rastrear se algum vídeo está em reprodução
-
-        // Função para configurar o comportamento da imagem e vídeo
-        function configurarComportamento(imagem, video, txtcard, contratarBtn) {
-            imagem.addEventListener("click", function() {
-                // Verificar se outros vídeos estão em reprodução
-                if (!videosEmReproducao) {
-                    videosEmReproducao = true; // Bloquear outros vídeos
-                    imagem.style.display = "none";
-                    video.style.display = "block";
-                    txtcard.style.display = "none";
-                    contratarBtn.style.display = "none";
-                    video.play();
-                }
-            });
-
-            video.addEventListener("ended", function() {
-                videosEmReproducao = false; // Desbloquear outros vídeos após o término
-                imagem.style.display = "block";
-                video.style.display = "none";
-                txtcard.style.display = "block";
-                contratarBtn.style.display = "block";
-            });
-        }
-
-        // Obter todas as cuidcards
-        var cuidcards = document.querySelectorAll(".cuidcard");
-
-        // Iterar sobre cada cuidcard e configurar o comportamento
-        cuidcards.forEach(function(cuidcard) {
-            var imagem = cuidcard.querySelector("img");
-            var video = cuidcard.querySelector("video");
-            var txtcard = cuidcard.querySelector(".txtcard");
-            var contratarBtn = cuidcard.querySelector("#saiba");
-
-            configurarComportamento(imagem, video, txtcard, contratarBtn);
-        });
-    </script>
-
     <!-- Biblioteca responsável pela realização das buscas -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.0/axios.min.js" integrity="sha512-WrdC3CE9vf1nBf58JHepuWT4x24uTacky9fuzw2g/3L9JkihgwZ6Cfv+JGTtNyosOhEmttMtEZ6H3qJWfI7gIQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
--- app/layouts/PerfilCuidador.php
+++ app/layouts/PerfilCuidador.php
@@ -57,7 +57,10 @@
                         </div>
                         <!--**3******-->
                         <div class="swiper-slide">
-                            <img src="images/3.jpg" />
+                            <video>
+                                <source src="video_cuidadora.mp4" type="video/mp4">
+                                Seu navegador não suporta o elemento de vídeo.
+                            </video>
                         </div>
                         <!--**4******-->
                         <div class="swiper-slide">
@@ -149,32 +152,47 @@
     <!-- Initialize Swiper -->
     <script>
         var swiper = new Swiper(".mySwiper", {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            450: {
+                slidesPerView: 2,
+                spaceBetween: 0,
             },
-            breakpoints: {
-                450: {
-                    slidesPerView: 2,
-                    spaceBetween: 0,
-                },
-                820: {
-                    slidesPerView: 1,
-                    spaceBetween: 0,
-                },
-                1024: {
-                    slidesPerView: 2,
-                    spaceBetween: 0,
-                },
+            820: {
+                slidesPerView: 1,
+                spaceBetween: 0,
             },
-        });
+            1024: {
+                slidesPerView: 2,
+                spaceBetween: 0,
+            },
+        },
+        on: {
+            slideChange: function () {
+                //Pausa o vídeo anterior quando o slide mudar
+                var previousSlide = swiper.slides[swiper.previousIndex];
+                if (previousSlide.querySelector("video")) {
+                    previousSlide.querySelector("video").pause();
+                }
+            },
+            slideChangeTransitionEnd: function () {
+                // Reproduz o vídeo quando o slide ficar ativo
+                var currentSlide = swiper.slides[swiper.activeIndex];
+                if (currentSlide.querySelector("video")) {
+                    currentSlide.querySelector("video").play();
+                }
+            },
+        },
+    });
 
-
-        $('.s-checkbox').on('change', function() {
-            $('.s-checkbox').not(this).prop('checked', false);
-        });
+    $('.s-checkbox').on('change', function () {
+        $('.s-checkbox').not(this).prop('checked', false);
+    });
     </script>
 </body>
 
